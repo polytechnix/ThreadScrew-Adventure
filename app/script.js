9a06e443fdc195a79763b5e763baacd168b24d00
polytechnix <113093740+polytechnix@users.noreply.github.com>
2023-09-01 23:56:12 +0300

    Update script.js
    I remember how to write in JS!

--- app/script.js
+++ app/script.js
@@ -1,1 +1,11 @@
+let hero = window.document.querySelector('#hero');
 
+const clickHandler = () => {
+  if(hero.style.backgroundColor === 'black') {
+    hero.style.backgroundColor === 'blue';
+  } else {
+    hero.style.backgroundColor === 'black';
+  }
+}
+
+hero.onclick = clickHandler;
